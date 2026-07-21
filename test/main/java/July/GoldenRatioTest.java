7de89142658c599e9eca20c9972fc417dc04461e
theProudWing <80337143+theProudWing@users.noreply.github.com>
2026-07-21 10:40:26 +0100

    [Clean] Removed instantiation of GoldenRatio utility class

--- test/main/java/July/GoldenRatioTest.java
+++ test/main/java/July/GoldenRatioTest.java
@@ -9,14 +9,12 @@ class GoldenRatioTest {
     @Test
     void isGoldenRatio() {
 
-        GoldenRatio checker = new GoldenRatio();
-
-        assertTrue(checker.isGoldenRatio(21, 34));
-        assertFalse(checker.isGoldenRatio(15, 20));
-        assertTrue(checker.isGoldenRatio(8, 13));
-        assertFalse(checker.isGoldenRatio(10, 16));
-        assertTrue(checker.isGoldenRatio(1618, 1000));
-        assertFalse(checker.isGoldenRatio(88, 55));
+        assertTrue(GoldenRatio.isGoldenRatio(21, 34));
+        assertFalse(GoldenRatio.isGoldenRatio(15, 20));
+        assertTrue(GoldenRatio.isGoldenRatio(8, 13));
+        assertFalse(GoldenRatio.isGoldenRatio(10, 16));
+        assertTrue(GoldenRatio.isGoldenRatio(1618, 1000));
+        assertFalse(GoldenRatio.isGoldenRatio(88, 55));
 
     }
 }
