dd1ca7a5915b92de7fcef4b726db09df200d5210
theProudWing <80337143+theProudWing@users.noreply.github.com>
2026-07-21 10:26:51 +0100

    [Structure] skeleton for 21-07-2026 challenge

---
+++ src/main/java/July/WordBlender.java
@@ -0,0 +1,24 @@
+/**
+ * Created by: Ben Jones
+ * Date:21/07/2026
+ * Time:10:21
+ */
+
+package main.java.July;
+
+public class WordBlender {
+
+    /***
+     * Given two words, return a new word by combining the first half of the first word
+     * with the second half of the second word.
+     * <p>
+     * For odd-length words, the first half is the shorter half.
+     *
+     * @param first the first word to blend, containing the first half of the result
+     * @param second the second word to blend, containing the second half of the result
+     * @return a new word combining the two halves derived from the input words.
+     */
+    public String blendWords(String first, String second){
+
+    }
+}
---
+++ test/main/java/July/WordBlenderTest.java
@@ -0,0 +1,12 @@
+package main.java.July;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WordBlenderTest {
+
+    @Test
+    void blendWords() {
+    }
+}
