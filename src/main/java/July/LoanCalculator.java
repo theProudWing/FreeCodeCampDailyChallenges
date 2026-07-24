5d0f1a1e9aff9b76df76ea142f82726f91638520
theProudWing <80337143+theProudWing@users.noreply.github.com>
2026-07-24 18:33:43 +0100

    [Complete] 24-07-2026 Challenge passes Test

--- src/main/java/July/LoanCalculator.java
+++ src/main/java/July/LoanCalculator.java
@@ -6,17 +6,49 @@
 
 package main.java.July;
 
+import java.util.ArrayList;
+
 public class LoanCalculator {
 
-    /***
-     * Given a loan amount, annual interest rate %, and fixed monthly payment, returns an array of the amount outstanding
-     * after each payment.
+    /**
+     * <p>Given a loan amount, annual interest rate %, and fixed monthly payment, returns an array of the amount outstanding
+     * after each payment.</p>
+     * <ul>
+     *     <li>Each month, interest is calculated on the remaining balance using the monthly interest rate:
+     *     (annual rate / 100) / 12, then the monthly payment is subtracted.</li>
+     *     <li>Return each remaining balance rounded to the nearest dollar.</li>
+     *     <li>Include the loan amount in the returned array. The first element in the array will always be the loan amount,
+     *     and the last element of the array will always be 0.</li>
+     * </ul>
      * @param amountBorrowed the total amount borrowed
-     * @param annualRate the annual interest rate, represented as a %
+     * @param annualInterestRate the annual interest rate, represented as a %
      * @param monthlyPayment the fixed amount to be paid back each month
-     * @return a forecast of outstanding amounts after each monthly payment until the loan is paid off.
+     * @return a forecast of outstanding amounts (rounded to the nearest dollarpound) after each monthly payment until the loan is paid off.
      */
-    public int[] getLoanSchedule(int amountBorrowed, int annualRate, int monthlyPayment){
-        return new int[0];
+    public static int[] getLoanSchedule(final double amountBorrowed, final double annualInterestRate, final double monthlyPayment){
+        // While loop, ends when outstanding amount <= 0
+        // Each loop:   Calculate interest
+        //              Add interest to outstanding amount
+        //              Reduce amount by monthlyPayment
+        //              Add outstanding balance (rounded) to Schedule ArrayList UNLESS less than 0, then add 0
+
+        double amountOutstanding = amountBorrowed;
+        double monthlyInterestRate = (annualInterestRate / 100) / 12;
+        ArrayList<Integer> paymentSchedule = new ArrayList<>();
+
+        paymentSchedule.add((int) Math.round(amountOutstanding));
+
+        while (amountOutstanding > 0){
+            double monthlyInterest = monthlyInterestRate * amountOutstanding; // calculate interest
+            amountOutstanding += monthlyInterest;                             // add interest to amount outstanding
+            amountOutstanding -= monthlyPayment;                              // make monthly payment
+            if (amountOutstanding < 0){
+                paymentSchedule.add(0);                                       // outstanding balance is 0
+            } else {
+                paymentSchedule.add((int) Math.round(amountOutstanding));     // add amount outstanding to schedule
+            }
+        }
+
+        return paymentSchedule.stream().mapToInt(i -> i).toArray();
     }
 }
--- test/main/java/July/LoanCalculatorTest.java
+++ test/main/java/July/LoanCalculatorTest.java
@@ -7,6 +7,29 @@ import static org.junit.jupiter.api.Assertions.*;
 class LoanCalculatorTest {
 
     @Test
-    void getLoanSchedule() {
+    void accurateLoanSchedules() {
+
+        assertArrayEquals(
+                new int[]{1000, 800, 600, 400, 200, 0},
+                LoanCalculator.getLoanSchedule(1000, 0, 200)
+        );
+        assertArrayEquals(
+                new int[]{1000, 804, 608, 410, 212, 13, 0},
+                LoanCalculator.getLoanSchedule(1000, 5, 200)
+        );
+        assertArrayEquals(
+                new int[]{10, 9, 9, 8, 8, 7, 6, 5, 5, 4, 3, 2, 1, 0, 0},
+                LoanCalculator.getLoanSchedule(10, 50, 1)
+        );
+        assertArrayEquals(
+                new int[] {5500, 5137, 4771, 4403, 4032, 3659, 3283, 2905, 2525, 2141, 1756, 1367, 977, 583, 187, 0},
+                LoanCalculator.getLoanSchedule(5500, 8, 400)
+        );
+        assertArrayEquals(
+                new int[] {50000, 48567, 47127, 45681, 44229, 42771, 41306, 39835, 38358, 36874, 35384, 33887, 32384,
+                        30874, 29358, 27835, 26306, 24770, 23227, 21678, 20122, 18559, 16990, 15413, 13830, 12240, 10643,
+                        9039, 7428, 5810, 4186, 2554, 915, 0},
+                LoanCalculator.getLoanSchedule(50_000, 5.2, 1650)
+        );
     }
 }
