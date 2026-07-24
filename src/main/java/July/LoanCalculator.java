2c1906b253bfcf01fbdf90567fb4a962def97a88
theProudWing <80337143+theProudWing@users.noreply.github.com>
2026-07-24 08:51:30 +0100

    [Outline] Created outline for LoanCalculator challenge.

---
+++ src/main/java/July/LoanCalculator.java
@@ -0,0 +1,22 @@
+/**
+ * Created by: Ben Jones
+ * Date:24/07/2026
+ * Time:08:44
+ */
+
+package main.java.July;
+
+public class LoanCalculator {
+
+    /***
+     * Given a loan amount, annual interest rate %, and fixed monthly payment, returns an array of the amount outstanding
+     * after each payment.
+     * @param amountBorrowed the total amount borrowed
+     * @param annualRate the annual interest rate, represented as a %
+     * @param monthlyPayment the fixed amount to be paid back each month
+     * @return a forecast of outstanding amounts after each monthly payment until the loan is paid off.
+     */
+    public int[] getLoanSchedule(int amountBorrowed, int annualRate, int monthlyPayment){
+        return new int[0];
+    }
+}
---
+++ test/main/java/July/LoanCalculatorTest.java
@@ -0,0 +1,12 @@
+package main.java.July;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoanCalculatorTest {
+
+    @Test
+    void getLoanSchedule() {
+    }
+}
