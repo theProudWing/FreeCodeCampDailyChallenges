a27f8778ee0756d8a75a50222f8b541ebf1033fe
theProudWing <80337143+theProudWing@users.noreply.github.com>
2026-07-27 18:06:57 +0100

    [Structure] Created Class and Test for 27-07-26 challenge - Pronic Number

---
+++ src/main/java/July/PronicNumber.java
@@ -0,0 +1,23 @@
+/**
+ * Created by: Ben Jones
+ * Date:27/07/2026
+ * Time:17:51
+ */
+
+package main.java.July;
+
+public class PronicNumber {
+
+    /**
+     * FreeCodeCamp Daily Challenge - 27-07-2026
+     * <p>
+     * Given a number, determines if that number is <b>pronic</b>, i.e., the product of two consecutive integers.
+     * <p>
+     * e.g., 6 is pronic because 2 * 3 = 6
+     * @param number
+     * @return
+     */
+    public static boolean isPronic(int number){
+        return true;
+    }
+}
---
+++ test/main/java/July/PronicNumberTest.java
@@ -0,0 +1,21 @@
+package main.java.July;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PronicNumberTest {
+
+    @Test
+    void correctlyIdentifiesPronicNumbers() {
+        assertTrue(PronicNumber.isPronic(6));
+        assertTrue(PronicNumber.isPronic(12));
+        assertTrue(PronicNumber.isPronic(132));
+        assertTrue(PronicNumber.isPronic(0));
+    }
+    @Test
+    void correctlyIdentifiesNonPronicNumbers() {
+        assertFalse(PronicNumber.isPronic(15));
+        assertFalse(PronicNumber.isPronic(80));
+    }
+}
