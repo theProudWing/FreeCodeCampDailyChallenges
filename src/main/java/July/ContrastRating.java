58ff3994040eff285dd2ffb4586fb6e4a5aea74c
theProudWing <80337143+theProudWing@users.noreply.github.com>
2026-07-28 14:50:22 +0100

    [Clean] Added JavaDoc for ContrastRating class

--- src/main/java/July/ContrastRating.java
+++ src/main/java/July/ContrastRating.java
@@ -6,7 +6,46 @@
 
 package main.java.July;
 
+
 public class ContrastRating {
+
+    /**
+     * Given a contrast ratio and a boolean indicating large text, return the Web Content Accessibility Guidelines (WCAG)
+     * rating using the following table:
+     * <table border="1" cellpadding="2">
+     *     <tr>
+     *         <th>Rating</th>
+     *         <th>Normal Text</th>
+     *         <th>Large Text</th></th>
+     *     </tr>
+     *     <tr>
+     *         <td>AAA</td>
+     *         <td>7.0+</td>
+     *         <td>4.5+</td>
+     *     </tr>
+     *     <tr>
+     *      <td>AA</td>
+     *      <td>4.5+</td>
+     *      <td>3.0+</td>
+     *     </tr>
+     *     <tr>
+     *         <td>Fail</td>
+     *         <td>below 4.5</td>
+     *         <td>below 3.0</td>
+     *     </tr>
+     * </table>
+     * <p>
+     *     Contrast Ratio is calculated as: (L1 + 0.05) / (L2 + 0.05) where:
+     *     <ul>
+     *         <li>L1 is the relative luminance of the lighter of the colours</li>
+     *         <li>L2 is the relative luminance of the darker of the colours</li>
+     *     </ul>
+     * </p>
+     * @param contrastRatio the ratio between the relative luminance of two colours
+     * @param isLargeText whether the text to display is of a large font size
+     * @return the WCAG rating given the provided parameters
+     * @throws IllegalArgumentException If the String provided for contrast ratio can not be converted to a double.
+     */
     public static String getContrastRating(String contrastRatio, boolean isLargeText) throws IllegalArgumentException{
         if (!isDouble(contrastRatio)){
             throw new IllegalArgumentException("Argument String cannot be converted to a double value.");
