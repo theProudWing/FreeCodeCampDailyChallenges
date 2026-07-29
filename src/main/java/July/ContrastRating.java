29f94f4b8951d41623223c93716012c894ab0e58
theProudWing <80337143+theProudWing@users.noreply.github.com>
2026-07-29 10:08:06 +0100

    [Complete] Tests passed for 29-07-2026 challenge - Contrast Rating 2

--- src/main/java/July/ContrastRating.java
+++ src/main/java/July/ContrastRating.java
@@ -10,6 +10,8 @@ package main.java.July;
 public class ContrastRating {
 
     /**
+     * FreeCodeCamp Daily Challenge - 28-07-2026
+     * <p>
      * Given a contrast ratio and a boolean indicating large text, return the Web Content Accessibility Guidelines (WCAG)
      * rating using the following table:
      * <table border="1" cellpadding="2">
@@ -62,7 +64,66 @@ public class ContrastRating {
             return "AA";
         }
         return "Fail";
+    }
 
+    /**
+     * FreeCodeCamp Daily Challenge - 29-07-2026
+     * <p>
+     * Given two relative luminance values and a boolean indicating whether the text is large,
+     * return the WCAG contrast rating based on the contrast ratio using the following table:
+     * <table border="1" cellpadding="2">
+     *     <tr>
+     *         <th>Rating</th>
+     *         <th>Normal Text</th>
+     *         <th>Large Text</th></th>
+     *     </tr>
+     *     <tr>
+     *         <td>AAA</td>
+     *         <td>7.0+</td>
+     *         <td>4.5+</td>
+     *     </tr>
+     *     <tr>
+     *      <td>AA</td>
+     *      <td>4.5+</td>
+     *      <td>3.0+</td>
+     *     </tr>
+     *     <tr>
+     *         <td>Fail</td>
+     *         <td>below 4.5</td>
+     *         <td>below 3.0</td>
+     *     </tr>
+     * </table>
+     * <p>
+     *     Contrast Ratio is calculated as: (L1 + 0.05) / (L2 + 0.05) where:
+     *     <ul>
+     *         <li>L1 is the relative luminance of the lighter of the colours</li>
+     *         <li>L2 is the relative luminance of the darker of the colours</li>
+     *     </ul>
+     * </p>
+     * @param lighter the lighter of the two luminance values
+     * @param darker the darker of the two luminance values
+     * @param isLargeText whether the text to display is of a large font size
+     * @return the WCAG rating given the provided parameters
+     * @throws IllegalArgumentException if lighter < darker
+     */
+    public static String getContrastRating(double lighter, double darker, boolean isLargeText) throws IllegalArgumentException{
+        if (lighter < darker){
+            throw new IllegalArgumentException(
+                    "The lighter of the two luminance values should be entered as the first argument."
+            );
+        }
+
+        double contrastRatio = (lighter + 0.05) / (darker + 0.05);
+
+        final double AAA_RATING = isLargeText ? 4.5 : 7.0;
+        final double AA_RATING = isLargeText ? 3.0 : 4.5;
+
+        if (contrastRatio >= AAA_RATING){
+            return "AAA";
+        } else if (contrastRatio >= AA_RATING){
+            return "AA";
+        }
+        return "Fail";
     }
 
     private static boolean isDouble(String string){
--- test/main/java/July/ContrastRatingTest.java
+++ test/main/java/July/ContrastRatingTest.java
@@ -1,5 +1,6 @@
 package main.java.July;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,12 +8,29 @@ import static org.junit.jupiter.api.Assertions.*;
 class ContrastRatingTest {
 
     @Test
-    void accuratelyDeterminesRating() {
+    @DisplayName("Accurate with String Param")
+    void accuratelyDeterminesRatingWithStringParams() {
         assertEquals("AAA", ContrastRating.getContrastRating("7.5", false));
         assertEquals("AA", ContrastRating.getContrastRating("4.8", false));
         assertEquals("Fail", ContrastRating.getContrastRating("4.2", false));
         assertEquals("AAA", ContrastRating.getContrastRating("4.5", true));
         assertEquals("AA", ContrastRating.getContrastRating("3.0", true));
         assertEquals("Fail", ContrastRating.getContrastRating("2.7", false));
+    }
+    @Test
+    @DisplayName("Accurate with Double Params")
+    void accuratelyDeterminesRatingWithDoubleParams(){
+        assertEquals("AAA", ContrastRating.getContrastRating(1.0, 0.0, false));
+        assertEquals("AA", ContrastRating.getContrastRating(0.9015, 0.1364, false));
+        assertEquals("Fail", ContrastRating.getContrastRating(0.8965, 0.1628, false));
+        assertEquals("AAA", ContrastRating.getContrastRating(0.7469, 0.0957, true));
+        assertEquals("AA", ContrastRating.getContrastRating(0.7489, 0.2018, true));
+        assertEquals("Fail", ContrastRating.getContrastRating(0.6571, 0.1974, true));
+    }
+    @Test
+    @DisplayName("Throws Exception with Illegal Arguments")
+    void throwsIllegalArgumentExceptionWithBadParams(){
+        assertThrows(IllegalArgumentException.class, () -> ContrastRating.getContrastRating("Ratio", true));
+        assertThrows(IllegalArgumentException.class, () -> ContrastRating.getContrastRating(0.0, 1.0, false));
     }
 }
