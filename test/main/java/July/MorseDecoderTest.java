1ceba704ef5bc857b6f8cbf67f44178f479c7b4d
theProudWing <80337143+theProudWing@users.noreply.github.com>
2026-07-31 10:13:03 +0100

    [Tests] Expanded tests for wider coverage of Morse Characters

--- test/main/java/July/MorseDecoderTest.java
+++ test/main/java/July/MorseDecoderTest.java
@@ -35,6 +35,15 @@ class MorseDecoderTest {
                 MorseDecoder.decodeMorse("..-. --- .-.   .-   --. --- --- -..   - .. -- .   -.-. .- .-.. .-.." +
                         "   .--- . -. -. -.--   --- -.   ---.. -.... --... ..... ...-- ----- ----.")
                 );
+        assertEquals(
+                "WHAT ARE YOU AFRAID OF?",
+                MorseDecoder.decodeMorse(".-- .... .- -   .- .-. .   -.-- --- ..-   .- ..-. .-. .- .. -.." +
+                        "   --- ..-. ..--..")
+        );
+        assertEquals("0 1 2 3 4 5 6 7 8 9,.?",
+                MorseDecoder.decodeMorse("-----   .----   ..---   ...--   ....-   .....   -....   --..." +
+                        "   ---..   ----. --..-- .-.-.- ..--..")
+        );
     }
 
     @Test
