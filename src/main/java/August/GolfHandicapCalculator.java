f6cfc87a4c5a10009bdd477e57c51733bb62230c
theProudWing <80337143+theProudWing@users.noreply.github.com>
2026-08-04 10:51:31 +0100

    [Structure] Class and Test Structure for 04-08-26: Golf Handicap Calculator

---
+++ src/main/java/August/GolfHandicapCalculator.java
@@ -0,0 +1,31 @@
+/**
+ * Created by: Ben Jones
+ * Date:04/08/2026
+ * Time:10:11
+ */
+
+package main.java.August;
+
+public class GolfHandicapCalculator {
+
+    /**
+     * FreeCodeCamp Daily Challenge - 04-08-2026
+     *
+     * Given an array of golf scores and a corresponding array of course par values, return the golfer's handicap determined by:
+     * <ol>
+     *     <li>Calculate the differential for each round by subtracting the
+     *     par value from the score.</li>
+     *     <li>Determine the average of all differentials, rounded to one decimal place</li>
+     *     <li>Return the average.</li>
+     * </ol>
+     *
+     * @param playerScores an array of golf scores for a single player.
+     * @param parValues the par scores for the courses corresponding to the golfer's scores.
+     * @return the golfer's handicap, rounded to 1 decimal place.
+     * @throws IllegalArgumentException if the parValue array is smaller than the playerScore array.
+     */
+    public static double calculateHandicap(final int[] playerScores, final int[] parValues) throws IllegalArgumentException{
+
+        return -1;
+    }
+}
---
+++ test/main/java/August/GolfHandicapCalculatorTest.java
@@ -0,0 +1,24 @@
+package main.java.August;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GolfHandicapCalculatorTest {
+
+    @Test
+    void accuratelyDeterminesHandicap() {
+        assertEquals(0, GolfHandicapCalculator.calculateHandicap(new int[]{72, 72, 72}, new int[]{72, 72, 72}));
+        assertEquals(6, GolfHandicapCalculator.calculateHandicap(new int[]{80, 76, 78, 78}, new int[]{72, 72, 72, 72}));
+        assertEquals(8.3, GolfHandicapCalculator.calculateHandicap(new int[]{42, 45, 46, 44}, new int[]{36, 36, 36, 36}));
+        assertEquals(8.3, GolfHandicapCalculator.calculateHandicap(new int[]{85, 80, 76, 79, 82}, new int[]{72, 72, 72, 71, 71}));
+        assertEquals(11.7, GolfHandicapCalculator.calculateHandicap(new int[]{41, 50, 48, 52, 46, 49}, new int[] {35, 37, 35, 37, 35, 37}));
+    }
+
+    @Test
+    void correctlyRejectsIllegalParameters() {
+        assertThrows(IllegalArgumentException.class, () -> GolfHandicapCalculator.calculateHandicap(new int[] {72, 72}, new int[] {87}));
+        assertThrows(IllegalArgumentException.class, () -> GolfHandicapCalculator.calculateHandicap(new int[] {72}, new int[] {87, 88}));
+        assertThrows(IllegalArgumentException.class, () -> GolfHandicapCalculator.calculateHandicap(new int[] {87, 90, 98, 107, 105}, new int[] {}));
+    }
+}
