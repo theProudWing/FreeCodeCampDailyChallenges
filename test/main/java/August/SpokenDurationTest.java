1699eea2512339bf111bda063db1502c93f1463a
theProudWing <80337143+theProudWing@users.noreply.github.com>
2026-08-05 10:23:47 +0100

    [Test] Created additional tests for 05-08-26: Spoken Duration

--- test/main/java/August/SpokenDurationTest.java
+++ test/main/java/August/SpokenDurationTest.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class SpokenDurationTest {
+class SpokenDurationTests {
 
     @Test
-    void getSpokenDuration() {
+    void accuratelyDeterminesDurationForPositiveParameters() {
         assertEquals("1 hour, 2 minutes, and 3 seconds", SpokenDuration.getSpokenDuration(3723));
         assertEquals("2 hours, 1 minute, and 35 seconds", SpokenDuration.getSpokenDuration(7295));
         assertEquals("2 hours, 22 minutes, and 1 second", SpokenDuration.getSpokenDuration(8521));
@@ -16,5 +16,22 @@ class SpokenDurationTest {
         assertEquals("20 hours", SpokenDuration.getSpokenDuration(72000));
         assertEquals("1 second", SpokenDuration.getSpokenDuration(1));
 
+
+    }
+
+    @Test
+    void accuratelyDeterminesDurationForNegativeParameters() {
+        assertEquals("1 hour, 2 minutes, and 3 seconds", SpokenDuration.getSpokenDuration(-3723));
+        assertEquals("2 hours, 1 minute, and 35 seconds", SpokenDuration.getSpokenDuration(-7295));
+        assertEquals("2 hours, 22 minutes, and 1 second", SpokenDuration.getSpokenDuration(-8521));
+        assertEquals("7 minutes, and 15 seconds", SpokenDuration.getSpokenDuration(-435));
+        assertEquals("4 hours, and 55 seconds", SpokenDuration.getSpokenDuration(-14455));
+        assertEquals("20 hours", SpokenDuration.getSpokenDuration(-72000));
+        assertEquals("1 second", SpokenDuration.getSpokenDuration(-1));
+    }
+
+    @Test
+    void accuratelyDeterminesDurationForZeroSeconds(){
+        assertEquals("0 seconds", SpokenDuration.getSpokenDuration(0));
     }
 }
