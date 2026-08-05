07b81621f69b2e6c4b1d445fc362275660f2a5b2
theProudWing <80337143+theProudWing@users.noreply.github.com>
2026-08-05 09:58:03 +0100

    [Test] Created test for 05-08-26: Spoken Duration

---
+++ test/main/java/August/SpokenDurationTest.java
@@ -0,0 +1,20 @@
+package main.java.August;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SpokenDurationTest {
+
+    @Test
+    void getSpokenDuration() {
+        assertEquals("1 hour, 2 minutes, and 3 seconds", SpokenDuration.getSpokenDuration(3723));
+        assertEquals("2 hours, 1 minute, and 35 seconds", SpokenDuration.getSpokenDuration(7295));
+        assertEquals("2 hours, 22 minutes, and 1 second", SpokenDuration.getSpokenDuration(8521));
+        assertEquals("7 minutes, and 15 seconds", SpokenDuration.getSpokenDuration(435));
+        assertEquals("4 hours, and 55 seconds", SpokenDuration.getSpokenDuration(14455));
+        assertEquals("20 hours", SpokenDuration.getSpokenDuration(72000));
+        assertEquals("1 second", SpokenDuration.getSpokenDuration(1));
+
+    }
+}
